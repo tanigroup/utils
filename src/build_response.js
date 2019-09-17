0c8dc9cc2a8398fb24e76d62d3328c0b8f1dc6b0
Keda87 <adiyatmubarak@gmail.com>
2019-09-17 15:23:30 +0700

    [FIX] Hardcoded min page and limit

--- src/build_response.js
+++ src/build_response.js
@@ -1,11 +1,9 @@
-const limit = 10;
-const minPage = 1;
-
 function buildResponse(data, params) {
   if (params) {
+    const { page = 1, limit = 10 } = params
     const { count: totalCount, rows: items } = data;
     const totalPages = Math.ceil(totalCount / limit) || 1;
-    const currentPage = Math.min(minPage, totalPages);
+    const currentPage = Math.min(page, totalPages);
 
     return {
       totalCount,
