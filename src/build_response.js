66864a1e3009ecba7a2e5b6815420f75f9a417d4
Yofriadi Yahya <yofriadiyahya@gmail.com>
2019-10-15 17:28:56 +0700

    remove camel case object

--- src/build_response.js
+++ src/build_response.js
@@ -1,22 +1,20 @@
-const camelCaseObj = require('./camel_case_object')
-
 function buildResponse(data, params) {
   if (params) {
-    const { page = 1, limit = 10 } = params
+    const { page = 1, limit = 10 } = params;
     const { count: totalCount, rows: items } = data;
     const totalPages = Math.ceil(totalCount / limit) || 1;
     const currentPage = Math.min(page, totalPages);
 
     return {
+      items,
+      params,
       totalCount,
       totalPages,
       currentPage,
-      params,
-      items: items.map((item) => camelCaseObj(item)),
     };
   }
 
-  return { data: camelCaseObj(data) };
+  return { data };
 }
 
 module.exports = buildResponse;
